d341c2492bbfed07573d1345a8e11a473fcd08c9
Simone Rizza <simonerizza@MacBook-Pro-de-Simone.local>
2022-11-29 12:26:05 +0100

    favicon path

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,7 +17,7 @@ export default {
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [{ rel: 'icon', type: 'image/png', href: '/favicon.png?' }],
+    link: [{ rel: 'icon', type: 'image/png', href: 'favicon.png?' }],
   },
 
   router: {
